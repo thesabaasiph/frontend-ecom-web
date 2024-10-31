f270fef67b47c700aaaeca3f807067a5d10d40ac
Asif <muhammad.asif@moniepoint.com>
2024-10-31 18:51:36 -0400

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
-        <div style={{width: 1440, height: 100, background:"#e6dbba" }}>
-           <div style={{display:"flex", flexDirection: "row", paddingInline: "54px", paddingBlock: "29px"}}>
-             <div>Furniro</div>
-             <div>hello JB</div>
+      <div style={{ width: 1440, height: 100, background: "#e6dbba" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            paddingInline: "54px",
+            paddingBlock: "29px",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
+            <img
+              src="logo.png"
+              alt="Logo"
+              style={{ width: "30px", height: "30px" }}
+            />
+            <span style={{ fontSize: "24px", fontWeight: "bold" }}>
+              Furniro
+            </span>
           </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "20px",
+              paddingLeft: "350px",
+              fontSize: "15px",
+              marginTop: "10px",
+            }}
+          >
+            <div>Home</div>
+            <div>Shop</div>
+            <div>About</div>
+            <div>Contact</div>
+          </div>
+        </div>
+      </div>
     </div>
-
-    </div>
-
   );
 }
 
